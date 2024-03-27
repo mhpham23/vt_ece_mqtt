fd10da47e0a15050e48b82ec661ada3f3911a0f3
mhpham23 <mhpham23@vt.edu>
2024-03-27 10:56:05 -0700

    connect client to wifi and test broker

--- client_1/client_1.ino
+++ client_1/client_1.ino
@@ -1,11 +1,16 @@
 // File name: client_1.ino
-// Decription: Initiating MQTT protocol for Arduino Client
+// Decription: Initiating MQTT protocol for Arduino Client. 128 bytes limit for package
 // Client Role: TBD
 // Library Used: ArduinoMqttClient, WifiNINA
 // Author: Hoan Pham (mhpham23@vt.edu)
 #include <ArduinoMqttClient.h>
 #include <WiFiNINA.h>
 #include "arduino_secrets.h"
+
+// Macros ////////////////////////////////////////////////////////////////////////////////
+#define BAUDRATE 9600
+#define DELAY_TIME 5000
+//////////////////////////////////////////////////////////////////////////////////////////
 
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h //////////////
 char ssid[] = SECRET_SSID;        // your network SSID (name)
@@ -26,12 +31,40 @@ WiFiClient wifiClient;
 MqttClient mqttClient(wifiClient);
 //////////////////////////////////////////////////////////////////////////////////////////
 
-void setup() {
+void setup() 
+{
   // put your setup code here, to run once:
+  //Initialize serial port
+  Serial.begin(BAUDRATE);
+
+  // attempt to connect to Wifi network:
+  Serial.print("Attempting to connect to WPA SSID: ");
+  Serial.println(ssid);
+  while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
+    // failed, retry
+    Serial.print(".");
+    delay(DELAY_TIME);
+  }
+
+  Serial.println("You're connected to the network");
+  Serial.println();
+
+  Serial.print("Attempting to connect to the MQTT broker: ");
+  Serial.println(broker);
+
+  if (!mqttClient.connect(broker, port)) {
+    Serial.print("MQTT connection failed! Error code = ");
+    Serial.println(mqttClient.connectError());
+
+    while (1);
+  }
+
+  Serial.println("You're connected to the MQTT broker!");
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
 
 }
