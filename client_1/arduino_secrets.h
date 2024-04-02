472e5187e4d767c42fb5279b5e0f81f1917c0a07
mhpham23 <mhpham23@vt.edu>
2024-04-02 10:14:51 -0700

    Add Roles, time tracking, pub tasks

--- client_1/arduino_secrets.h
+++ client_1/arduino_secrets.h
@@ -2,7 +2,16 @@
 // Decription: Store WIFI name and password
 // Author: Hoan Pham (mhpham23@vt.edu)
 
+//Network information//////////////////////////////////////
 #define SECRET_SSID "TMOBILE-C471" // Network Name
 #define SECRET_PASS "12345-hlgt4y" // Network Password
 #define MQTT_IP "192.168.12.104" // Local Host's IP address
 #define LISTENING_PORT 1883 // Listening port on local host
+///////////////////////////////////////////////////////////
+
+
+//Client Roles////////////////////////////////////////////
+#define PUB 1
+#define SUB 2
+#define BOTH 3
+/////////////////////////////////////////////////////////
--- client_1/client_1.ino
+++ client_1/client_1.ino
@@ -10,6 +10,8 @@
 // Macros ////////////////////////////////////////////////////////////////////////////////
 #define BAUDRATE 9600
 #define DELAY_TIME 5000
+#define MSG_INTERVAL 8000
+#define CLIENT_ROLE PUB
 //////////////////////////////////////////////////////////////////////////////////////////
 
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h //////////////
@@ -17,19 +19,29 @@ char ssid[] = SECRET_SSID;        // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
 ///////////////////////////////////////////////////////////////////////////////////////////
 
-// Declaring topics //////////////////////////////////////////////////////////////////////
+//Network Information/////////////////////////////////////////////////////////////////////
 const char broker[] = "test.mosquitto.org";
 const char localhost_broker[] = MQTT_IP;
 int        port     = LISTENING_PORT;
+//////////////////////////////////////////////////////////////////////////////////////////
+
+// Declaring topics //////////////////////////////////////////////////////////////////////
 const char topic[]  = "real_unique_topic";
 const char topic2[]  = "real_unique_topic_2";
 const char topic3[]  = "real_unique_topic_3";
 //////////////////////////////////////////////////////////////////////////////////////////
 
 
-//Constructors ///////////////////////////////////////////////////////////////////////////
+// Constructors ///////////////////////////////////////////////////////////////////////////
 WiFiClient wifiClient;
 MqttClient mqttClient(wifiClient);
+//////////////////////////////////////////////////////////////////////////////////////////
+
+// Time tracking /////////////////////////////////////////////////////////////////////////
+//set interval for sending messages (milliseconds)
+const long interval = MSG_INTERVAL;
+unsigned long previousMillis = 0;
+int count = 0;
 //////////////////////////////////////////////////////////////////////////////////////////
 
 void setup() 
@@ -67,6 +79,51 @@ void setup()
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // call poll() regularly to allow the library to send MQTT keep alive which
+  // avoids being disconnected by the broker
+  mqttClient.poll();
+
+  unsigned long currentMillis = millis();
+
+  /* Message task starts here */
+  if (currentMillis - previousMillis >= interval) 
+  {
+    // save the last time a message was sent
+    previousMillis = currentMillis;
+
+    //record random value from A0, A1 and A2
+    int Rvalue = analogRead(A0);
+    int Rvalue2 = analogRead(A1);
+    int Rvalue3 = analogRead(A2);
+
+    Serial.print("Sending message to topic: ");
+    Serial.println(topic);
+    Serial.println(Rvalue);
+
+    Serial.print("Sending message to topic: ");
+    Serial.println(topic2);
+    Serial.println(Rvalue2);
+
+    Serial.print("Sending message to topic: ");
+    Serial.println(topic2);
+    Serial.println(Rvalue3);
+
+    // send message, the Print interface can be used to set the message contents
+    mqttClient.beginMessage(topic);
+    mqttClient.print(Rvalue);
+    mqttClient.endMessage();
+
+    mqttClient.beginMessage(topic2);
+    mqttClient.print(Rvalue2);
+    mqttClient.endMessage();
+
+    mqttClient.beginMessage(topic3);
+    mqttClient.print(Rvalue3);
+    mqttClient.endMessage();
+
+    Serial.println();
+  } /* END TASK */
+
 
 
 }
